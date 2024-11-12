6ef0a3677f2952ef6487c2ac782ce1981e35599f
PoojaSharmasuper <dhakadsonu196@gmail.com>
2024-11-12 10:07:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "RahulReviewsYT"
+API_ID       = 28690893
+API_HASH     = "c214f988aa1ac0b998ace0b7cd0e215f"
+BOT_TOKEN    = "7527149215:AAENWRjcoMm-2RM1saga5JwtV8E4yIuxFHk"
+SESSION      = "BQD6zg0ACAkYIBVTJkoEaPaiwoWomk6bcGzrpOsFMkFRJp5e0IrDliAu2D-zfM-0vxBEFhWPfC2VHmCx5IjMUyvEZfbBwxM1Zszv6EezbAGlKkhg66IpnxALr5KeO7g-2kbHoRsvul0amWJKa_V0LHTmxY2BPNV_adHlFCyVLO2RVrtd9hc0kVaXEMhDxlaXmAEQZNNeK3EYutYkLJk9OqFT4pprY3SlDM13DCNcdP1OWEPV0WCDnOx6UGFJjaepVQ4vrM6DSQhnBK_gM11SNPOmnNGzkV7BkvRvzmJezudVZ2W8dMh_F9tPU0YCV1Cty-Hj5-gdchAnHJOtCglZj8VOsv8_xgAAAAG6nhFKAA"
+DATABASE_URI = "mongodb+srv://AutoSender:AutoSender@cluster0.qdalj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002322401361
+ADMIN        = 1454524346
+CHANNEL      = "super4crezy"
