3bd89b1677b9c4eb5cf513f5743255d30296ba0a
PrajwalPatil-01 <prajwalpatilxp@gmail.com>
2024-12-01 10:29:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "c214f988aa1ac0b998ace0b7cd0e215f"
 BOT_TOKEN    = "7527149215:AAENWRjcoMm-2RM1saga5JwtV8E4yIuxFHk"
 SESSION      = "1BVtsOJ8Bu4lNN5k_1NfRb07wuyi2HrJktJH1NGxgrUd5C8qFSMP0Aqc9QBkDxbXLwZVad9oZ8xFhaPLrioNSbZDxvQ6QuPC8kYxcfZLpsMlwzaLkiW21QM_w2T1UtIq8on9jRBbIA0mgq79nXUX4I_QkpHpDXdHAUOqwT7_oRMXB1D1aeAODAKaBowhj1aOn4C2x_NVtqI_tuwnP8quVzbHj1cJQgnvWkvl1NuY90GyNqFy0WJrzVNKRdOSKTMGbBJ2sUYywpP4D4Wv1c22jrlApNArA6DWSxP1FOKwoYYxL08cLWTspOoLxdMk6Wzg66eIuEjexYUovL2Ipvtaor5oLQ83JKjk="
 DATABASE_URI = "mongodb+srv://AutoSender:AutoSender@cluster0.qdalj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1002322401361
+LOG_CHANNEL  = -1002263541423
 ADMIN        = 1454524346
-CHANNEL      = "super4crezy"
+CHANNEL      = "xp_prajwal"
